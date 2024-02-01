b17f9d98aa0ad6723c6732a4fae336d007ea66f2
Tomás Fernandes <fernandesstomas@gmail.com>
2024-02-01 15:27:08 +0000

    Section2 btn style done

--- blip-concept-website/src/HomePageSections/Section2.js
+++ blip-concept-website/src/HomePageSections/Section2.js
@@ -1,4 +1,15 @@
 export default function Section2() {
+  const arrowRight = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 -960 960 960"
+      width="24"
+    >
+      <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
+    </svg>
+  );
+
   return (
     <section className="section-2">
       <div className="section-2-grid">
