0342a041cf38512421c62b80df2bafd024248ca8
Tomás Fernandes <fernandesstomas@gmail.com>
2024-02-06 16:32:18 +0000

    first ctaSection queries

--- blip-concept-website/src/HomePageItems/ImageSlider.js
+++ blip-concept-website/src/HomePageItems/ImageSlider.js
@@ -1,13 +1,43 @@
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css"; // Import Swiper styles
 
 export default function ImageSlider() {
-  const images = [
+  // Define images for different screen sizes
+  const [images, setImages] = useState([
     { id: "1", img: "/img/blip-office-1.webp" },
     { id: "2", img: "/img/blip-office-2.webp" },
     { id: "3", img: "/img/blip-office-3.webp" },
     { id: "4", img: "/img/blip-office-4.webp" },
-  ];
+  ]);
+
+  // Function to update images based on screen width
+  const updateImages = () => {
+    const maxWidth = window.matchMedia("(max-width: 75em)").matches;
+    console.log("maxWidth:", maxWidth);
+    const newImages = maxWidth
+      ? [
+          { id: "1", img: "/img/blip-office-1-smaller-screen.webp" },
+          { id: "2", img: "/img/blip-office-2-smaller-screen.webp" },
+          { id: "3", img: "/img/blip-office-3-smaller-screen.webp" },
+          { id: "4", img: "/img/blip-office-4-smaller-screen.webp" },
+        ]
+      : [
+          { id: "1", img: "/img/blip-office-1.webp" },
+          { id: "2", img: "/img/blip-office-2.webp" },
+          { id: "3", img: "/img/blip-office-3.webp" },
+          { id: "4", img: "/img/blip-office-4.webp" },
+        ];
+    console.log("newImages:", newImages);
+    setImages(newImages);
+  };
+
+  // Update images on initial render and when screen width changes
+  useEffect(() => {
+    updateImages();
+    window.addEventListener("resize", updateImages);
+    return () => window.removeEventListener("resize", updateImages);
+  }, []);
 
   return (
     <div className="slider-div">
